455a227c2213c71ffc3bb218d828d201ccef2bf7
Lurese <papiyoung49@gmail.com>
2024-11-11 17:22:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// alert('Hello World!')
+
+// const name = "Tester"
+
+var myName ="Lurese"
+console.log(myName)
+let Name ="john doe"
+console.log(Name)
+const job ="QA Tester"
+console.log(job)
+var school ="LASU"
+console.log(school)
+let mySchool = "STA"
+console.log(mySchool)
+const School ="EMU"
+console.log(School)
