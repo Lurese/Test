ca34ab4655a2f5d4ee7ee088196f62f42b4275ce
Lurese <papiyoung49@gmail.com>
2024-11-21 06:57:50 +0200

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// alert('Hello World!')
-
-// const name = "Tester"
-
-var myName ="Lurese"
-console.log(myName)
-let Name ="john doe"
-console.log(Name)
-const job ="QA Tester"
-console.log(job)
-var school ="LASU"
-console.log(school)
-let mySchool = "STA"
-console.log(mySchool)
-const School ="EMU"
-console.log(School)
+// var myName ="Lurese"
+// console.log(myName)
+// let Name ="john doe"
+// console.log(Name)
+// const job ="QA Tester"
+// console.log(job)
+// var school ="LASU"
+// console.log(school)
+// let mySchool = "STA"
+// console.log(mySchool)
+// const School ="EMU"
+// console.log(School)
+const day ="Tuesday"
+const number = 2
+const output ="Today is " + day + "." + " It is the number " + number + " day of the week" + "."
+console.log(output)
