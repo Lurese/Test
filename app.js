a44054688180713bfb7459f6eb8390ae11d00802
Lurese <papiyoung49@gmail.com>
2024-12-14 20:33:51 +0200

    first commit

--- app.js
+++ app.js
@@ -15,26 +15,26 @@
 // const output ="Today is " + day + "." + " It is the number " + number + " day of the week" + "."
 // console.log(output)
 
-const number = 10
-if (number % 2 === 0){
-    console.log('Number is even')
-}
-else{
-    console.log('Number is even')
-}
+// const number = 10
+// if (number % 2 === 0){
+//     console.log('Number is even')
+// }
+// else{
+//     console.log('Number is even')
+// }
 
-const myNumber = 5
-if(myNumber % 2 === 0){
-    console.log('Number is even')
-}
-else{
-    console.log('Number is odd')
-}
+// const myNumber = 5
+// if(myNumber % 2 === 0){
+//     console.log('Number is even')
+// }
+// else{
+//     console.log('Number is odd')
+// }
 
 const side1 = 6
 const side2 = 5
 const side3 = 6
-if(side1 ===  side2 === side3){
+if(side1 ===  side2 && side2 === side3){
     console.log('Equilateral triangle')
 } else if(side1 === side2 || side1 === side3 || side2 === side3){
     console.log('Isoscele triangle')
@@ -43,14 +43,43 @@ else{
     console.log('Scalene triangle')
 }
 
-let odd = 1 // starting point
-while(odd <=20) {
-    console.log(odd)
-    odd = odd + 2
-}
+// let odd = 1 // starting point
+// while(odd <=20) {
+//     console.log(odd)
+//     odd = odd + 2
+// }
  
-let oddOnes = 1 // starting point
-for(; oddOnes <= 20;){
-    console.log(oddOnes)
-    oddOnes = oddOnes + 2
-}
+// let oddOnes = 1 // starting point
+// for(; oddOnes <= 20;){
+//     console.log(oddOnes)
+//     oddOnes = oddOnes + 2
+// }
+
+
+
+// const number = 19
+// if (number % 2 === 0){
+//     console.log('Number is even')
+// }
+// else{
+//     console.log ('Number is odd')
+// }
+
+// const side1 = 3
+// const side2 = 3
+// const side3 = 3
+// if(side1 === side2 === side3){
+//     console.log('Equilateral triangle')
+// } else if( side1 === side2 || side1 === side3 || side2 === side3){
+//     console.log('Isoceles triangle')
+// }
+// else{
+//     console.log('Scalene triangle')
+// }
+
+// let odd = 1 // starting point
+// while ( odd <=20){
+//     console.log(odd + 'star')
+//     odd= odd + 1
+// }
+    
